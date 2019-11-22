d3355d7c9a8cde043e5325adb164aac7f5af0d9f
PIYATHIDA CHOOJAN <61010656@kmitl.ac.th>
2019-11-22 13:44:46 +0700

    change parh

--- datacomtest/detectPic.py
+++ datacomtest/detectPic.py
@@ -19,8 +19,8 @@ def Black_White(path,pathsaveBW):
     #cv2.imshow('Image crop',img_binary)
     cv2.imwrite(pathsaveBW, img_binary)
 
+
 def getValue_pixel(path):
-    count=0
     imV = cv2.imread(path, 0) 
     p = imV.shape
     rows,cols = imV.shape
@@ -30,52 +30,63 @@ def getValue_pixel(path):
                 imV[y,x]=1
             else:
                 imV[y,x]=0
-            print(imV[y,x], end = '')
-        print("\t")
+            #print(imV[y,x], end = '')
+        #print("\t")
     
     if ((imV[0][0]==0 and imV[1][1]==0 and imV[2][2]==0 and imV[3][3]==0 and imV[1][0]==0 
     and imV[2][0]==0 and imV[2][1]==0 and imV[3][0]==0 and imV[3][1]==0 and imV[3][2]==0 ) 
     or ( imV[1][0]==0  and imV[2][0]==0 and imV[2][1]==0 and imV[3][0]==0 and imV[3][1]==0 and imV[3][2]==0 and imV[2][2]==1 ) 
     or (imV[2][0]==0 and imV[3][1]==0 and imV[3][0]==0 and imV[2][1]==1)):
-        count+=1
+        for y in range(rows):
+            for x in range(cols):
+                print(imV[y,x], end = '')
+            print("\t")
         print('lower')
     elif ((imV[0][0]==0 and imV[1][1]==0 and imV[2][2]==0 and imV[3][3]==0 and imV[0][1]==0 
     and imV[0][2]==0 and imV[0][3]==0 and imV[1][2]==0 and imV[1][3]==0 and imV[2][3]==0 ) 
     or ( imV[0][1]==0  and imV[0][2]==0 and imV[0][3]==0 and imV[1][2]==0 and imV[1][3]==0 and imV[2][3]==0 and imV[1][1]==1) 
     or (imV[0][2]==0 and imV[0][3]==0 and imV[1][3]==0 and imV[1][2]==1)):
-        count+=1
+        for y in range(rows):
+            for x in range(cols):
+                print(imV[y,x], end = '')
+            print("\t")
         print('upper')
     elif ((imV[0][0]==0 and imV[1][0]==0 and imV[2][0]==0 and imV[3][0]==0 and imV[0][1]==1 )
     or (imV[0][0]==0 and imV[1][0]==0 and imV[2][0]==0 and imV[3][0]==0 
     and imV[0][1]==0 and imV[1][1]==0 and imV[2][1]==0 and imV[3][1]==0 ) 
     or (imV[1][0]==0 and imV[2][0]==0 and imV[3][0]==0 )) :
-        count+=1
+        for y in range(rows):
+            for x in range(cols):
+                print(imV[y,x], end = '')
+            print("\t")
         print('left')
     elif ((imV[0][2]==0 and imV[1][2]==0 and imV[2][2]==0 and imV[3][2]==0 
     and imV[0][3]==0 and imV[1][3]==0 and imV[2][3]==0 and imV[3][3]==0  )
     or (imV[0][3]==0 and imV[1][3]==0 and imV[2][3]==0 and imV[3][3]==0 and imV[0][2]==1) 
     or (imV[1][3]==0 and imV[2][3]==0 and imV[3][3]==0 )) :
-        count+=1
+        for y in range(rows):
+            for x in range(cols):
+                print(imV[y,x], end = '')
+            print("\t")
         print('right')
     elif ((imV[0][0]==0 and imV[0][1]==0 and imV[0][2]==0 and imV[0][3]==0 
     and imV[1][0]==0 and imV[1][1]==0 and imV[1][2]==0 and imV[1][3]==0 )
     or (imV[0][0]==0 and imV[0][1]==0 and imV[0][2]==0 and imV[0][3]==0)): 
-        count+=1
+        for y in range(rows):
+            for x in range(cols):
+                print(imV[y,x], end = '')
+            print("\t")
         print('top')
     elif ((imV[2][0]==0 and imV[2][1]==0 and imV[2][2]==0 and imV[2][3]==0 
     and imV[3][0]==0 and imV[3][1]==0 and imV[3][2]==0 and imV[3][3]==0 )
     or (imV[3][0]==0 and imV[3][1]==0 and imV[3][2]==0 and imV[3][3]==0)): 
-        count+=1
+        for y in range(rows):
+            for x in range(cols):
+                print(imV[y,x], end = '')
+            print("\t")
         print('bottom') 
     else:
-        print('error') 
-        print('------------------------')
-        print('new pic')
-        path_original = r'C:/out/2.png'
-        Crop(x,y,h,w,path_original,path_crop)
-        Resize(path_crop,path_RZ)
-        Black_White(path_RZ,path_BW)
-        getValue_pixel(path_BW)
+        return 1
     #print('W H :',p)
     #cv2.imshow('Imagefor get value =',imgV)
 
@@ -96,6 +107,7 @@ def Pule():
 def TakePic():
     os.chdir(path_cam)
     subprocess.check_output('java code.SimpleRead')
+
 def TakeBegin(comservo,comcam):
     while(True):
         if(comservo.inWaiting()):
@@ -110,7 +122,7 @@ def TakeBegin(comservo,comcam):
     arduino_servo.close()
 #-------------------------------------------------------------
 #main 
-path_original = r'C:/out/0.bmp'
+path_original = r'C:/out/4.bmp'
 path_crop=r'C:/image/crop/test.png'
 path_BW=r'C:/image/b-w/bw .png'
 path_RZ= r'C:/image/crop/resize/h.png'
@@ -127,7 +139,19 @@ Crop(x,y,h,w,path_original,path_crop)
 Resize(path_crop,path_RZ)
 Black_White(path_RZ,path_BW)
 getValue_pixel(path_BW)
-
+if getValue_pixel(path_BW)==1:
+    path_original = r'C:/out/4.bmp'
+    Crop(x,y,h,w,path_original,path_crop)
+    Resize(path_crop,path_RZ)
+    Black_White(path_RZ,path_BW)
+    getValue_pixel(path_BW)
+    if getValue_pixel(path_BW)==1:
+        path_original = r'C:/out/5.bmp'
+        Crop(x,y,h,w,path_original,path_crop)
+        Resize(path_crop,path_RZ)
+        Black_White(path_RZ,path_BW)
+        getValue_pixel(path_RZ)
+ 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 #TakePic()
